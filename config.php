ccf35302d4f1faa2b0a46f3c92d9df358229d904
AleksandrTararumenko <aleksandr.tararumenko@ilogos-ua.com>
2014-11-21 11:35:04 +0200

    add new file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define("URL_ADRESS", "http://cosmotop.ru/serv_yml.php");
+define("DIR_LIB", "lib/");
+define("DIR_LOAD", "load/");
+define("DBHOST", 'localhost');
+define("DBNAME", 'parser');
+define("DBUSER", 'moon');
+define("DBPASS", 'moon');
+?>
