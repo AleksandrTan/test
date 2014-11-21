818892b7fec3016ffcdf6ae50391a74b944bfa30
AleksandrTararumenko <aleksandr.tararumenko@ilogos-ua.com>
2014-11-21 11:04:40 +0200

    delet

--- tpl/cp.categories.rebuild.tpl.php
+++
@@ -1,6 +0,0 @@
-Перестроить категории <?php print $provider;?>?
-<br /><br />
-<form method="post">
-    <input type="hidden" name="categories-rebuild" value="1" />
-    <input type="submit" value="Перестроить" />&nbsp;&nbsp;<a href="/parser">Отмена</a>
-</form>
